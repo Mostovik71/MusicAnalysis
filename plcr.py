c15142837bdc75c0d7879185a9c098d1866f45e3
Mostovik71 <56130198+Mostovik71@users.noreply.github.com>
2021-08-16 22:00:23 +0300

    Add files via upload

--- plcr.py
+++ plcr.py
@@ -11,9 +11,9 @@ cache_token = token.get_access_token()
 sp = spotipy.Spotify(cache_token)
 #playlist_creator = "spotify"
 #playlist_id = "37i9dQZF1EM9Lq3EzxxeM3"
-playlist_creator2 = "Mostovik"
-playlist_id2="5p3sjl2UqWTrhvUYPYvyt6"
-
+playlist_creator2 = "Spotify"
+#playlist_id2="5p3sjl2UqWTrhvUYPYvyt6"
+playlist_id2='37i9dQZF1DWTXGqmP0bfT3'
 #sp.user_playlist_tracks("spotify", "37i9dQZF1DWTcqUzwhNmKv")
 pd.set_option('display.max_rows', None)
 pd.set_option('display.max_columns', None)
@@ -54,25 +54,8 @@ def analyze_playlist(creator, playlist_id):
     return playlist_df
 
 playlist_df = analyze_playlist(playlist_creator2, playlist_id2)
-playlist_df['duration_min'] = pd.Series()
-playlist_df['user'] = pd.Series()
-playlist_df['user_score'] = pd.Series()
 
-for i in range(len(playlist_df['duration_ms'])):
-    playlist_df['duration_min'][i]=playlist_df['duration_ms'][i] / 60000
-    playlist_df['user_score'][i] = round(random.uniform(1,5),1)
-    playlist_df['user'][i]=random.randint(1,5)
-playlist_df.drop(['duration_ms'], axis = 1,inplace=True)
-playlist_df.drop(['key'], axis = 1,inplace=True)
-playlist_df.drop(['mode'], axis = 1,inplace=True)
-playlist_df.drop(['liveness'], axis = 1,inplace=True)
-playlist_df.drop(['loudness'], axis = 1,inplace=True)
-playlist_df.drop(['instrumentalness'], axis = 1,inplace=True)
-playlist_df.drop(['track_id'], axis = 1,inplace=True)
-playlist_df.drop(['tempo'], axis = 1,inplace=True)
-playlist_df.drop(['speechiness'], axis = 1,inplace=True)
-for i in range(len(playlist_df['popularity'])):
-    playlist_df['popularity'][i]=playlist_df['popularity'][i] / 100
+
 #print(playlist_df)
-playlist_df.to_csv('25')
+playlist_df.to_csv('energy')
 #print(tabulate(playlist_df,headers='keys',tablefmt='psql'))
